3082bdeadf54bbf73bc4c9790f7c134a56eccd5d
skdksldk1485 <skdksldk1485@naver.com>
2022-05-29 22:36:36 +0900

    feat : 로그인/로그아웃 기능 구현 - 레이아웃 공통화 적용 - 로그인 성공 시 메인페이지 이동 - 회원가입 버튼 클릭시 회원가입 페이지 이동 - 로그인 성공 시 네비게이션바에 로그아웃 메뉴 활성화 - 로그아웃 버튼 클릭시 로그아웃 기능 동작 및 로그인 메뉴 활성화

--- src/main/java/com/shop/projectlion/domain/member/service/MemberService.java
+++ src/main/java/com/shop/projectlion/domain/member/service/MemberService.java
@@ -4,13 +4,17 @@ import com.shop.projectlion.domain.member.entity.Member;
 import com.shop.projectlion.domain.member.repository.MemberRepository;
 import com.shop.projectlion.domain.member.validation.MemberValidation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final MemberValidation memberValidation;
@@ -18,6 +22,21 @@ public class MemberService {
     public Member saveMember(Member member){
         memberValidation.validateDuplicateMember(member);
         return memberRepository.save(member);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 
 }
--- src/main/java/com/shop/projectlion/web/login/controller/LoginController.java
+++ src/main/java/com/shop/projectlion/web/login/controller/LoginController.java
@@ -26,6 +26,12 @@ public class LoginController {
         return "login/loginform";
     }
 
+    @GetMapping("/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg","아이디 또는 비밀번호를 확인해주세요");
+        return "login/loginform";
+    }
+
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("memberRegisterDto", new MemberRegisterDto());
