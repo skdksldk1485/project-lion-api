caaadcd195f41c74cece5386bf618ce67350f853
skdksldk1485 <skdksldk1485@naver.com>
2022-06-06 02:56:01 +0900

    refactor: 주석 추가

--- src/test/java/com/shop/projectlion/ProjectLionApplicationTests.java
+++ src/test/java/com/shop/projectlion/ProjectLionApplicationTests.java
@@ -3,6 +3,7 @@ package com.shop.projectlion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//deploy를 대비한 주석처리
 //@SpringBootTest
 class ProjectLionApplicationTests {
 
