821fbd0345639dc79f7b5850f45afcb74e700d63
skdksldk1485 <skdksldk1485@naver.com>
2022-06-06 02:27:23 +0900

    refactor: deploy.yml 수정

--- src/test/java/com/shop/projectlion/ProjectLionApplicationTests.java
+++ src/test/java/com/shop/projectlion/ProjectLionApplicationTests.java
@@ -3,7 +3,7 @@ package com.shop.projectlion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProjectLionApplicationTests {
 
     @Test
